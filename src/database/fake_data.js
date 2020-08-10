9999ad77f841efd7c5659cc9d4a868a34e472e9e
rafahmarinho <rafael.marinho96@hotmail.com>
2020-08-10 01:04:45 -0300

    site completo c/ backend

---
+++ src/database/fake_data.js
@@ -0,0 +1,12 @@
+
+const proffys = [{
+    name: 'Rafael Marinho',
+    avatar:"https://avatars2.githubusercontent.com/u/64975112?s=460&u=86012823c153746e3d667dc8791d9eca0fe4f31c&v=4",
+    whatsapp:'11955337788',
+    bio:'Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões',
+    subject:'Química',
+    cost: '20',
+    weekday:[0],
+    time_from:[720],
+    time_to:[1220],
+}]
---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const server = express()
+const nunjucks = require('nunjucks')
+const {pageLanding, pageStudy, pageGiveClasses, saveClasses} = require('./pages')
+
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+
+server
+    .use(express.urlencoded({ extended: true}))
+    .use(express.static('public'))
+    .get('/', pageLanding)
+    .get('/study', pageStudy)
+    .get('/give-classes', pageGiveClasses)
+    .post('/save-classes', saveClasses)
+    .listen(5500)
